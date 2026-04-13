bc59851522729d8aefb1e447208933d937c218c3
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2026-04-13 10:09:42 +0100

    addedthesitelogo

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -21,3 +21,30 @@ function toggle() {
     mainMenu.classList.toggle("open");
     mainMenu.classList.toggle("d-none");
 }
+
+const themeToggle = document.getElementById('theme-toggle');
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+// Check for saved theme preference
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode');
+  sunIcon.style.display = 'none';
+  moonIcon.style.display = 'block';
+}
+
+
